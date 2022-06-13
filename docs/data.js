634640272d21cac923709bfa63de4cd73a090bc3
github-action-benchmark <github@users.noreply.github.com>
2022-06-13 07:51:43 +0000

    add Benchmark (pytest) benchmark result for 559545a5c402721f6bb946ac1e0ba45f153d8962

---
+++ docs/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1655106703879,
+  "repoUrl": "https://github.com/sondrfos/test_dependabot",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "sondre.foslien@gmail.com",
+            "name": "sondrfos",
+            "username": "sondrfos"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "559545a5c402721f6bb946ac1e0ba45f153d8962",
+          "message": "Update benchmarks.yaml",
+          "timestamp": "2022-06-13T09:50:45+02:00",
+          "tree_id": "32ee2c9e640562d01afa79b47e5114ad8b8f12f5",
+          "url": "https://github.com/sondrfos/test_dependabot/commit/559545a5c402721f6bb946ac1e0ba45f153d8962"
+        },
+        "date": 1655106702246,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "benchmark.py::test_clustering_performance_cluster_every_frame",
+            "value": 0.19979567823444258,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000022620634118017593",
+            "extra": "mean: 5.00511326789856 sec\nrounds: 5"
+          }
+        ]
+      }
+    ]
+  }
+}
